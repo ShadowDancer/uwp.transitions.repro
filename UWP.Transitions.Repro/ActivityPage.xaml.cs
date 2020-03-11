cc33aed2fb2a58340eaf574217ad05ee68f774de
Przemysław Onak <przemekonak@gmail.com>
2020-03-11 09:17:34 +0100

    Repro

---
+++ UWP.Transitions.Repro/ActivityPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UWP.Transitions.Repro
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ActivityPage : Page
+    {
+        public ActivityPage()
+        {
+            this.InitializeComponent();
+            SetState(false);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SetState(true);
+        }
+
+        private void SetState(bool working)
+        {
+            Button.Visibility = working ? Visibility.Collapsed : Visibility.Visible;
+            Spinner.Visibility = working ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            SetState(false);
+            base.OnNavigatingFrom(e);
+        }
+    }
+}
